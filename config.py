4ebcfda27fb13302f4b712ece4eb3be1aba048af
Abuzhan Aidaraliyev <abuzhan.aidaraliyev@gmail.com>
2023-11-13 00:41:04 +0100

    Try to fix db connection

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ class ProductionConfig(Config):
         self.DB_CONNECTION_PARAMETERS = {
             'user': self.DB_USER,
             'password': service_secrets.DB_PASSWORD,
-            'port': self.DB_PORT,
+            'port': 5433,
             'host': '127.0.0.1',
             'dbname': self.DB_NAME,
         }
